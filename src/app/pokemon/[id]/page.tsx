35e2795df77ea9270f9845c6463c57c7d603a0a1
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2024-07-06 09:31:41 +0530

    some changes

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { notFound } from 'next/navigation';
+import Image from 'next/image';
 
 interface PokemonDetail {
+  id: number; // Add this field to use for National Dex number
   species: {
     url: string;
   };
@@ -25,6 +27,12 @@ interface PokemonDetail {
   }[];
   abilities: {
     ability: {
+      name: string;
+    };
+    is_hidden: boolean;
+  }[];
+  types: {
+    type: {
       name: string;
     };
   }[];
@@ -64,6 +72,27 @@ interface Props {
   };
 }
 
+const typeImages: { [key: string]: string } = {
+  normal: '/types/normal.png',
+  fire: '/types/fire.png',
+  water: '/types/water.png',
+  grass: '/types/grass.png',
+  electric: '/types/electric.png',
+  ice: '/types/ice.png',
+  fighting: '/types/fighting.png',
+  poison: '/types/poison.png',
+  ground: '/types/ground.png',
+  flying: '/types/flying.png',
+  psychic: '/types/psychic.png',
+  bug: '/types/bug.png',
+  rock: '/types/rock.png',
+  ghost: '/types/ghost.png',
+  dragon: '/types/dragon.png',
+  dark: '/types/dark.png',
+  steel: '/types/steel.png',
+  fairy: '/types/fairy.png',
+};
+
 export default async function PokemonPage({ params }: Props) {
   const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${params.id}`);
   if (!response.data) {
@@ -87,13 +116,23 @@ export default async function PokemonPage({ params }: Props) {
   const getStatColor = (stat: number) => {
     if (stat > 100) return 'bg-green-600';
     if (stat > 70) return 'bg-yellow-600';
+    if (stat > 40) return 'bg-orange-600';
+    if (stat > 0) return 'bg-red-600';
+    if (stat === 0) return 'bg-gray-600';
     return 'bg-red-600';
+  };
+
+  const getTotalStatColor = (total: number) => {
+    if (total > 500) return 'bg-blue-600';
+    if (total > 400) return 'bg-purple-600';
+    if (total > 300) return 'bg-teal-600';
+    return 'bg-pink-600';
   };
 
   const totalBaseStats = pokemon.stats.reduce((acc, stat) => acc + stat.base_stat, 0);
 
   return (
-    <div className="p-6 h-full bg-hero flex flex-col md:flex-row justify-around items-center">
+    <div className="p-6 h-full bg-hero flex flex-col md:flex-row justify-center items-center">
       <div>
         <div className="relative w-52 h-52 mx-auto mb-4">
           <div className="absolute inset-0 flex justify-center border-2 border-gray-300 items-center z-0 bg-bg2 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 opacity-40">
@@ -108,40 +147,70 @@ export default async function PokemonPage({ params }: Props) {
             </div>
           </div>
           <img
-            src={pokemon.sprites.other['official-artwork'].front_default} 
-            alt={pokemon.name} 
+            src={pokemon.sprites.other['official-artwork'].front_default}
+            alt={pokemon.name}
             className="w-52 h-52 object-cover object-center relative"
           />
         </div>
-        <div className='px-20  flex items-center justify-center'>
-        <p className="text-center text-gray-100 mt-8 p-4 border border-white rounded-lg">{description}</p>
+        <h1 className='text-4xl font-bold text-gray-950 mb-4 text-center'>
+        #{pokemon.id}
+        </h1>
+        <h1 className="text-4xl font-bold text-gray-950 mb-4 text-center">
+           {pokemon.name}
+        </h1>
+
+        <div className="flex justify-center gap-2 mb-4">
+          {pokemon.types.map((type, idx) => (
+            <Image
+              key={idx}
+              src={typeImages[type.type.name] || '/types/default.png'}
+              alt={type.type.name}
+              width={40}
+              height={40}
+              className="object-cover"
+            />
+          ))}
+        </div>
+
+        <div className="px-20 flex items-center justify-center">
+          <p className="text-center text-white mt-2 p-4 border border-white rounded-lg">{description}</p>
         </div>
       </div>
       <div className="ml-10 border border-gray-300 rounded-lg shadow-lg p-6 max-w-md text-center relative overflow-hidden">
         <div className="text-left mb-6">
-          <h2 className="text-2xl font-bold text-gray-800 mb-4">Stats</h2>
+          <h2 className="text-2xl font-bold text-gray-950 mb-4">Stats</h2>
           <ul className="space-y-2">
             {pokemon.stats.map((stat, index) => (
               <li key={index} className="flex items-center">
-                <span className="font-semibold text-gray-700 capitalize w-24">{stat.stat.name}:</span>
+                <span className="font-semibold text-gray-800 capitalize w-24">{stat.stat.name}:</span>
                 <div className="relative w-48 h-4 bg-gray-200 rounded-full overflow-hidden">
                   <div className={`absolute h-full ${getStatColor(stat.base_stat)}`} style={{ width: `${stat.base_stat}%` }}></div>
                 </div>
                 <span className="ml-4 w-8 text-right">{stat.base_stat}</span>
               </li>
             ))}
-            <li className="font-semibold text-gray-700">Total Base Stats: <span className='text-white font-bold'>{totalBaseStats}</span></li>
+            <li className="flex items-center">
+              <span className="font-semibold text-gray-800 capitalize w-24">Total Base Stats:</span>
+              <div className="relative w-48 h-4 bg-gray-200 rounded-full overflow-hidden">
+                <div className={`absolute h-full ${getTotalStatColor(totalBaseStats)}`} style={{ width: `${(totalBaseStats / 600) * 100}%` }}></div>
+              </div>
+              <span className="w-16 ml-2 text-center text-white">{totalBaseStats}</span>
+            </li>
           </ul>
         </div>
         <div className="text-left mb-6">
-          <h2 className="text-2xl font-bold text-gray-800 mb-4">Abilities</h2>
+          <h2 className="text-2xl font-bold text-gray-950 mb-4">Abilities</h2>
           <ul className="space-y-1">
             {pokemon.abilities.map((ability, index) => (
-              <li key={index} className="capitalize">{ability.ability.name}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
-}
+                            <li key={index} className="capitalize">
+                            {ability.ability.name}
+                            {ability.is_hidden && <span className="ml-2 text-sm text-gray-200">(Hidden)</span>}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              );
+            }
+            
