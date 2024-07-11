dfbf5b7275ec8580c48d67ff13fed59676fe5604
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2024-07-11 09:02:33 +0530

    some changes

--- src/components/Generate/Preview.tsx
+++ src/components/Generate/Preview.tsx
@@ -1,9 +1,9 @@
 "use client";
-
 import Image from "next/image";
 import React, { forwardRef } from "react";
 import { CardContainer, CardBody, CardItem } from "../ui/3d-card";
 import clsx from "clsx";
+import { FaDownload } from "react-icons/fa";
 
 interface PokemonCardProps {
   name: string;
@@ -154,23 +154,12 @@ const Preview = forwardRef<HTMLDivElement, PokemonCardProps>(
           <span className="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
           <span className="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
           <span className="relative z-20 flex items-center text-sm">
-            <svg
-              className="relative w-5 h-5 mr-2 text-white"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 10V3L4 14h7v7l9-11h-7z"
-              ></path>
-            </svg>
+           <div className="flex items-center justify-between gap-2">
+           <FaDownload/>
             <button onClick={onDownload} className="z-30">
               Download Pokémon
             </button>
+           </div>
           </span>
         </div>
       </div>
--- src/components/team/ImageUpload.tsx
+++ src/components/team/ImageUpload.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaDownload } from 'react-icons/fa';
 
 interface ImageUploadProps {
   handleImageUpload: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -16,12 +17,18 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ handleImageUpload, downloadIm
         className="mb-4"
       />
      </div>
-      <button
-        onClick={downloadImage}
-        className="bg-green-500 text-white px-4 py-2 rounded"
-      >
-        Download Team with Image
-      </button>
+      <div className="box-border relative z-30 inline-flex items-center justify-center w-auto px-8 py-3 overflow-hidden font-bold text-white transition-all duration-300 bg-indigo-600 rounded-md cursor-pointer group ring-offset-2 ring-1 ring-indigo-300 ring-offset-indigo-200 hover:ring-offset-indigo-500 ease focus:outline-none">
+          <span className="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+          <span className="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+          <span className="relative z-20 flex items-center text-sm">
+           <div className="flex items-center justify-between gap-2">
+           <FaDownload/>
+            <button  onClick={downloadImage} className="z-30">
+              Download Pokémon
+            </button>
+           </div>
+          </span>
+        </div>
     </div>
   );
 };
