db647176515f31b56909c3258a268b6d5e968056
MihirJaiswal <jaiswalmihir.business@gmail.com>
2024-12-31 12:44:58 +0530

    some changes

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -6,10 +6,10 @@ import logo from '../../public/logo.webp'
 
 const HomeHeader = () => {
   return (
-    <div className='fixed w-full top-0 z-50 bg-mainb backdrop-blur-sm border-b border-gray-600 lg:backdrop-blur-sm'>
-        <div className='flex justify-between items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4'>
+    <div className='fixed w-full top-0 z-50 bg-mainb backdrop-blur-sm border-b border-gray-600'>
+      <div className='flex justify-between items-center px-5 lg:px-7.5 xl:px-10'>
         <a href="/" className='flex items-center w-[12rem] xl:mr-8'>
-            <Image
+          <Image
             alt='logo'
             src={logo}
             width={52}
@@ -17,17 +17,21 @@ const HomeHeader = () => {
             loading='lazy'
             placeholder='blur'
             quality={100}
-            className='max-sm:size-10 m-2'
-            />
-            <p className='text-gray-200 font-bold text-2xl max-lg:hidden uppercase'>MonsterPédia</p>
+            className='m-2'
+          />
+          <p className='text-gray-200 font-bold text-2xl hidden lg:block uppercase'>
+            MonsterPédia
+          </p>
         </a>
-        <div>
-        <HomeNav/>
+        
+        <div className='hidden lg:block'>
+          <HomeNav />
         </div>
-      <div className='flex-between gap-5'>
-         <HomeMobileNav/>
+
+        <div className='lg:hidden flex items-center gap-5'>
+          <HomeMobileNav />
+        </div>
       </div>
-    </div>  
     </div>
   )
 }
--- src/components/HomeMobileNav.tsx
+++ src/components/HomeMobileNav.tsx
@@ -1,73 +1,78 @@
 'use client'
 import React from 'react'
 import {
-    Sheet,
-    SheetClose,
-    SheetContent,
-    SheetTrigger,
-  } from "@/components/ui/sheet"
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 import Image from 'next/image'
 import Link from 'next/link'
-import{ usePathname } from 'next/navigation'
-import{ cn } from '@/lib/utils'
-import{ navigation } from '../../constant/index'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils'
+import { navigation } from '../../constant/index'
 import Hamburger from '../../public/hamburger.svg'
 import logo from '../../public/logo.webp'
 
 function HomeMobileNav() {
-    const pathname = usePathname();
+  const pathname = usePathname();
+
   return (
-  <section className='w-full max-w-[264px] z-50'>
-   <div className='flex flex-col md:hidden'>
-    <Sheet>
-      <SheetTrigger asChild>
-        <Image 
-        src={Hamburger}
-        width={36}
-        height={36}
-        alt="hamburger"
-        loading='lazy'
-        quality={100}
-        className='cursor-pointer sm:hidden' 
-        />
-      </SheetTrigger>
-      <SheetContent side='right' className='bg-black bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-5 border-gray-700'>
-      <Link href='/' className='flex items-center gap-1'>
-     <Image
-      src={logo}
-      width={42}
-      height={42}
-      alt='logo'
-      loading='lazy'
-      placeholder='blur'
-      quality={100}
-      className='max-sm:size-10'
-      />
-      <p className='text-[21px] font-bold text-white px-2 mt-1'>MonsterPédia</p>
-     </Link>
-    <div className='flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto'>
-        <SheetClose asChild>
-            <section className='flex h-full flex-col gap-6 pt-16 text-white'>
-            <div className='flex flex-1 flex-col gap-6'>
-            {navigation.map((link) => {
-                const isActive = pathname === link.route;
-              return(
-                    <SheetClose asChild key={link.route}>
-                    <Link href={link.route} key={link.label} className={cn("flex gap-4 items-center p-4 rounded-lg w-full max-w-60", {'bg-cyan-600' : isActive,})}>
-                      <p className='font-semibold text-[18px] '>{link.title}</p>
-                    </Link>
-                    </SheetClose>
-                )
-            })}
-           </div>
-          </section>
-         </SheetClose>
-        </div>
-        </SheetContent>
-      </Sheet>
-     </div>
+    <section className='w-full max-w-[264px] z-50'>
+      <div className='flex flex-col md:hidden'>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Image
+              src={Hamburger}
+              width={36}
+              height={36}
+              alt="hamburger"
+              loading='lazy'
+              quality={100}
+              className='cursor-pointer sm:hidden'
+            />
+          </SheetTrigger>
+          <SheetContent side='right' className='bg-black bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-5 border-gray-700'>
+            <Link href='/' className='flex items-center gap-1'>
+              <Image
+                src={logo}
+                width={42}
+                height={42}
+                alt='logo'
+                loading='lazy'
+                placeholder='blur'
+                quality={100}
+                className='max-sm:size-10'
+              />
+              <p className='text-[21px] font-bold text-white px-2 mt-1'>MonsterPédia</p>
+            </Link>
+            <div className='flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto'>
+              <SheetClose asChild>
+                <section className='flex h-full flex-col gap-6 pt-16 text-white'>
+                  <div className='flex flex-1 flex-col gap-6'>
+                    {navigation.map((link) => {
+                      const isActive = pathname === link.route;
+                      return (
+                        <Link
+                          href={link.route}
+                          key={link.route} // Ensure each Link has a unique key
+                          className={cn("flex gap-4 items-center p-4 rounded-lg w-full max-w-60", {
+                            'bg-cyan-600': isActive,
+                          })}
+                        >
+                          <p className='font-semibold text-[18px]'>{link.title}</p>
+                        </Link>
+                      )
+                    })}
+                  </div>
+                </section>
+              </SheetClose>
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
     </section>
   )
 }
 
-export default HomeMobileNav
+export default HomeMobileNav;
